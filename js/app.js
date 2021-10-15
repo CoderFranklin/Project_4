598bf0a39826fd893f74e0bd0b0c2dd72d677b6e
CoderFranklin <82669390+CoderFranklin@users.noreply.github.com>
2021-10-15 13:42:36 -0500

    update

--- js/app.js
+++ js/app.js
@@ -20,22 +20,18 @@ let totalLoan,
   monthlyTotal,
   labels = ["Principal & Interest", "Property Tax", "Home Insurance", "HOA"], // Will show in the pie chart.
   backgroundColor = [
-    "rgba(255, 99, 132, 0.2)",
-    "rgba(255, 159, 64, 0.2)",
-    "rgba(255, 205, 86, 0.2)",
-    "rgba(75, 192, 192, 0.2)",
-    "rgba(54, 162, 235, 0.2)",
-    "rgba(153, 102, 255, 0.2)",
-    "rgba(201, 203, 207, 0.2)",
+    "rgba(255, 99, 132, 1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 205, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
   ];
 borderColor = [
-  "rgba(255, 99, 132, 0.2)",
-  "rgba(255, 159, 64, 0.2)",
-  "rgba(255, 205, 86, 0.2)",
-  "rgba(75, 192, 192, 0.2)",
-  "rgba(54, 162, 235, 0.2)",
-  "rgba(153, 102, 255, 0.2)",
-  "rgba(201, 203, 207, 0.2)",
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 205, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
 ];
 //Removes characters and returns only numbers
 function getNumber(str) {
@@ -67,20 +63,31 @@ let myChart = new Chart(ctx, {
 myChart.options.animation = false;
 
 //Add event lister to inputs
-let i,
-  inputTexts = document.getElementsByClassName("form-group__textInput");
+/*let i,
+inputTexts = document.getElementsByClassName("form-group__textInput");
 for (i = 0; i < inputTexts.length; i++) {
   //This loop is for multiple forms.
   //console.log(inputTexts[i])
   inputTexts[i].addEventListener("input", updateInputsState);
-} // Try to merge the for loops into one loop.
+} // Try to merge the for loops into one loop.*/
 
-inputSlides = document.getElementsByClassName("form-group__range-slide");
+/*inputSlides = document.getElementsByClassName("form-group__range-slide");
 for (i = 0; i < inputSlides.length; i++) {
   //This loop is for multiple forms.
   //console.log(inputSlides[i])
   inputSlides[i].addEventListener("input", updateInputsState); //Trigger the updateInputState
+}*/
+
+let inputSlides = document.getElementsByClassName("form-group__range-slide"),
+  inputTexts = document.getElementsByClassName("form-group__textInput");
+
+for (let inputText of inputTexts) {
+  inputText.addEventListener("input", updateInputsState);
 }
+for (let inputSlide of inputSlides) {
+  inputSlide.addEventListener("input", updateInputsState);
+}
+
 function updateInputsState(event) {
   let name = event.target.name;
   let value = event.target.value;
@@ -91,7 +98,7 @@ function updateInputsState(event) {
   if (event.target.type == "range") {
     let total = (document.getElementsByClassName(
       `total__${name}`
-    )[0].innerHTML = `${value}`); //only one element in idex.html
+    )[0].innerHTML = `${value}`); //only one element in index.html
   }
   state = {
     //Taking all the properties from state.
@@ -131,15 +138,36 @@ function calculateDate() {
     parseFloat(monthlyPropertyTaxes) +
     parseFloat(monthlyHomeInsurance) +
     parseFloat(monthlyHOA);
-
-
-    document.getElementsByClassName('info__numbers--principal')[0].innerHTML = parseFloat(monthlyPrincipalInterest).toFixed(2);
-    document.getElementsByClassName('info__numbers--property_taxes')[0].innerHTML = parseFloat(monthlyPropertyTaxes).toFixed(2);
-    document.getElementsByClassName('info__numbers--home_insurance')[0].innerHTML = parseFloat(monthlyHomeInsurance).toFixed(2);
-    document.getElementsByClassName('info__numbers--hoa')[0].innerHTML = parseFloat(monthlyHOA).toFixed(2);
-    document.getElementsByClassName('info__numbers--total')[0].innerHTML = (monthlyTotal).toFixed(2);
-
-
-  console.log(monthlyTotal);
+  document.getElementsByClassName("info__numbers--principal")[0].innerHTML =
+    parseFloat(monthlyPrincipalInterest).toFixed(2);
+  document.getElementsByClassName(
+    "info__numbers--property_taxes"
+  )[0].innerHTML = parseFloat(monthlyPropertyTaxes).toFixed(2);
+  document.getElementsByClassName(
+    "info__numbers--home_insurance"
+  )[0].innerHTML = parseFloat(monthlyHomeInsurance).toFixed(2);
+  document.getElementsByClassName("info__numbers--hoa")[0].innerHTML =
+    parseFloat(monthlyHOA).toFixed(2);
+  document.getElementsByClassName("info__numbers--total")[0].innerHTML =
+    monthlyTotal.toFixed(2);
+  updateChart(myChart, labels, backgroundColor);
 }
-console.log(inputTexts);
+
+//Update Chart
+function updateChart(chart, label, color) {
+  chart.data.datasets.pop();
+  chart.data.datasets.push({
+    labels: labels,
+    backgroundColor: color,
+    data: [
+      monthlyPrincipalInterest,
+      monthlyInterest,
+      monthlyPropertyTaxes,
+      monthlyHomeInsurance,
+      monthlyHOA,
+    ],
+  });
+  chart.options.transitions.active.animation.duration = 0;
+  chart.update();
+}
+calculateDate;
