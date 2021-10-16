b7596f11eb660bd2b148d119d0d4cab41a2d8298
Doug Franklin <82669390+CoderFranklin@users.noreply.github.com>
2021-10-15 20:51:21 -0500

    Update app

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ let totalLoan,
     "rgba(54, 162, 235, 1)",
     "rgba(255, 205, 86, 1)",
     "rgba(75, 192, 192, 1)",
-    "rgba(153, 102, 255, 1)",
+    "rgba(153, 102, 255, 1)"
   ];
 borderColor = [
   "rgba(255, 99, 132, 1)",
@@ -161,10 +161,10 @@ function updateChart(chart, label, color) {
     backgroundColor: color,
     data: [
       monthlyPrincipalInterest,
-      monthlyInterest,
-      monthlyPropertyTaxes,
+      monthlyPropertyTaxes,   
       monthlyHomeInsurance,
       monthlyHOA,
+      monthlyInterest,
     ],
   });
   chart.options.transitions.active.animation.duration = 0;
